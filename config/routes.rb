5e9c8b0c7eeb2456e1d479a27ce9bf95b17405b5
aatela <sami@aatela.fi>
2015-03-02 18:13:15 +0200

    Viikko7

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
 
   resources :beer_clubs
 
-  resources :users
+  resources :users do
+    post 'toggle_frozen', on: :member
+  end
 
+  get 'ngbeerlist', to:'beers#nglist'
+  get 'beerlist', to:'beers#list'
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'
@@ -15,7 +19,9 @@ Rails.application.routes.draw do
 
   resources :beers
 
-  resources :breweries
+  resources :breweries do
+    post 'toggle_activity', on: :member
+  end
 
   resources :ratings, only: [:index, :new, :create, :destroy]
 
